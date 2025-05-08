fef26bd85dab01889084c1b092f7764284ad2d4e
Gsszs <gabriel.selvenca@icloud.com>
2025-05-08 16:52:35 -0300

    feat: Botão tarefas inferior

--- S1 - Taskool (ok)/HomeForm.Designer.cs
+++ S1 - Taskool (ok)/HomeForm.Designer.cs
@@ -49,6 +49,8 @@
             this.label7 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button8 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel2.SuspendLayout();
@@ -72,7 +74,7 @@
             this.panel1.Controls.Add(this.label8);
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Size = new System.Drawing.Size(1289, 830);
+            this.panel1.Size = new System.Drawing.Size(1288, 669);
             // 
             // pictureBox1
             // 
@@ -87,6 +89,8 @@
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.button8);
             this.panel2.Controls.Add(this.label2);
             this.panel2.Controls.Add(this.label1);
             this.panel2.Location = new System.Drawing.Point(0, 0);
@@ -119,7 +123,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
-            this.label5.Location = new System.Drawing.Point(311, 550);
+            this.label5.Location = new System.Drawing.Point(304, 394);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(126, 46);
             this.label5.TabIndex = 14;
@@ -129,7 +133,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F);
-            this.label4.Location = new System.Drawing.Point(309, 473);
+            this.label4.Location = new System.Drawing.Point(302, 317);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(152, 55);
             this.label4.TabIndex = 13;
@@ -138,7 +142,7 @@
             // button5
             // 
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.button5.Location = new System.Drawing.Point(319, 778);
+            this.button5.Location = new System.Drawing.Point(312, 622);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(106, 34);
             this.button5.TabIndex = 12;
@@ -151,7 +155,7 @@
             this.label10.AutoSize = true;
             this.label10.BackColor = System.Drawing.Color.Transparent;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label10.Location = new System.Drawing.Point(315, 706);
+            this.label10.Location = new System.Drawing.Point(308, 550);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(51, 20);
             this.label10.TabIndex = 16;
@@ -162,7 +166,7 @@
             this.label6.AutoSize = true;
             this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label6.Location = new System.Drawing.Point(315, 652);
+            this.label6.Location = new System.Drawing.Point(308, 496);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(51, 20);
             this.label6.TabIndex = 15;
@@ -172,7 +176,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label8.Location = new System.Drawing.Point(431, 785);
+            this.label8.Location = new System.Drawing.Point(424, 629);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(51, 20);
             this.label8.TabIndex = 17;
@@ -181,7 +185,7 @@
             // button7
             // 
             this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.button7.Location = new System.Drawing.Point(1215, 768);
+            this.button7.Location = new System.Drawing.Point(1208, 612);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(62, 51);
             this.button7.TabIndex = 19;
@@ -192,7 +196,7 @@
             // button6
             // 
             this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.button6.Location = new System.Drawing.Point(1147, 767);
+            this.button6.Location = new System.Drawing.Point(1140, 611);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(62, 51);
             this.button6.TabIndex = 18;
@@ -275,11 +279,31 @@
             // 
             this.timer1.Interval = 1000;
             // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button8.Location = new System.Drawing.Point(12, 627);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(35, 35);
+            this.button8.TabIndex = 25;
+            this.button8.Text = "+";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label9.Location = new System.Drawing.Point(66, 636);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(67, 20);
+            this.label9.TabIndex = 26;
+            this.label9.Text = "Projetos";
+            // 
             // HomeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1289, 830);
+            this.ClientSize = new System.Drawing.Size(1288, 669);
             this.Name = "HomeForm";
             this.Text = "HomeForm";
             this.Load += new System.EventHandler(this.HomeForm_Load);
@@ -316,5 +340,7 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button8;
     }
 }
